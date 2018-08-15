ffdd19e3e69a3850c29bf7eda99576ae961b8587
Orlando Nascimento <orlandocorreia2@hotmail.com>
2018-08-15 03:57:11 -0300

    generate combinations

--- factorial.php
+++ factorial.php
@@ -1,4 +1,14 @@
 <?php
+
+function dd(...$args) {
+    echo '<pre>';
+    foreach ($args as $arg) {
+        var_dump(print_r($arg));
+    }
+    echo '</pre>';
+    die();
+}
+
 $packageOne = [
     "package" => "internet",
     "ids" => [1, 2, 3, 4, 5]
@@ -19,83 +29,88 @@ $packageFour = [
     "ids" => [41, 42, 43, 44, 45]
 ];
 
-function dd(...$args) {
-    echo '<pre>';
-    foreach ($args as $arg) {
-        var_dump(print_r($arg));
-    }
-    echo '</pre>';
-    die();
-}
+//Variavel será utilizada para definir o tamanho da primeira coluna
+$base_array_length = 0;
 
-
-function combination(&$object, $package, $total)
+function combinateBaseColumn($result_combinations_packages, $rest_packages, $external_loop, $internal_loop)
 {
-    for ($i = 0; $i < $total; $i++) {
-        foreach ($package['ids'] as $k => $id) {
-            for ($j = 0; $j < count($package['ids']); $j++) {
-                echo "$id<br/>";
-                $aux = [$package['package'] => $id];
-                if (!isset($object[$i])) {
-                    $object[$i] = $aux;
-                } else {
-                    $object[$i] = array_merge($aux, $object[$i]);
+//    Variável para não permitir que exceda o índice da coluna base
+    global $base_array_length;
+
+//    Índice da coluna base para atribuir as combinações
+    $index = 0;
+
+//    Loop exponencial externo
+    for ($i = 0; $i < $external_loop; $i++) {
+        foreach ($rest_packages[0]['ids'] as $rest) {
+//            Loop exponencial interno
+            for ($j = 0; $j < $internal_loop; $j++) {
+                if ($index < $base_array_length) {
+                    $result_combinations_packages[$index] .= ' | ' . $rest_packages[0]['package'] . ', ' . $rest;
                 }
-                $i++;
+
+                $index++;
             }
         }
     }
 
+//    Recalcula os loops exponenciais para ser utilizado caso esta função seja chamada recursivamente
+    $external_loop *= 5;
+    $internal_loop /= 5;
+
+//    Retira o item corrente do resto dos pacotes
+    array_shift($rest_packages);
+
+//    Se tiver pacotes restantes chama esta função recursivamente
+    if ($rest_packages) {
+        combinateBaseColumn($result_combinations_packages, $rest_packages, $external_loop, $internal_loop);
+    } else {
+//        Término do processo, imprime na tela o resultado das combinações
+        dd($result_combinations_packages);
+    }
 }
 
-function factorial(...$packages)
+
+
+function generateCombinations(...$packages)
 {
-    $object = [];
-    foreach ($packages as $package) {
-        $total = count($package['ids']) *  count($package['ids']);
-        combination($object, $package, $total);
-    }
-    dd($object);
-    exit;
+//    Tamanho base da primeira coluna
+    global $base_array_length;
 
+//    Array da primeira coluna
+    $first_package = [];
 
+//    Pega a quantidade de pacotes tirando o da primeira coluna
+    $count_packages = count($packages) -1;
 
+//    Pega a quantidade de itens de cada pacote no caso 5
+    $count_package_id = count($packages[0]['ids']);
 
+//    Faz o calculo da exponenciação para gerar o loop conforme a quantidade de pacotes tirando o da primeira coluna
+    $exp = pow($count_package_id, $count_packages);
 
-
-    if (count($packages) > 0 ) {
-        $object = [];
-        for($i = 0; $i < count($packages); $i++) {
-            if (count($packages[$i]["ids"]) > 0) {
-                foreach ($packages[$i]["ids"] as $k => $id) {
-                    if (isset($packages[$i + 1])) {
-                        $nextPackage = $packages[$i + 1];
-                        foreach ($nextPackage["ids"] as $y => $nextId) {
-                            if (isset($object[$k])) {
-                                $object[$k] = array_merge($object[$k], [$nextPackage["package"] => $nextId]);
-                            } else {
-                                $object[$k] = [$packages[$i]["package"] => $id];
-                            }
-                            //$object[$y] = [$packages[$i]["package"] => $nextId];
-                        }
-                    }
-
-
-//                    if (isset($packages[$i + 1])) {
-//                        foreach ($packages[$i + 1] as $y => $package) {
-//                            dd($packages[$i], $packages[$i + 1]);
-//                            if (isset($object[$k])) {
-//                                $object[$k] = array_merge($object[$k], [$packages[$i]["package"] => $id]);
-//                            } else {
-//                                $object[$k] = [$packages[$i]["package"] => $id];
-//                            }
-//                        }
-//                    }
-                }
-            }
+//    Realiza o loop nos itens do primeiro pacote que é o base
+    foreach ($packages[0]['ids'] as $id) {
+        for ($i = 0; $i < $exp; $i++) {
+            $first_package[] = $packages[0]['package'].' - '.  $id;
         }
-        dd($object);
     }
+
+//    Exclui a primeira coluna dos packages
+    array_shift($packages);
+
+//    Calculo exponencial dos loops para atribuir a coluna base
+//    Loop externo do array
+    $external_loop = count($packages) > 1 ? pow(5, count($packages) -1) : 5;
+//    Loop interno do array
+    $internal_loop = pow(5, count($packages) -1);
+//    Seta o tamanho da coluna base
+    $base_array_length = count($first_package);
+
+//    Função para atribuir a coluna base as combinações
+    combinateBaseColumn($first_package, $packages, $external_loop, $internal_loop);
 }
 
-var_dump(factorial($packageOne, $packageThree), factorial($packageOne, $packageTwo, $packageThree), factorial($packageOne, $packageTwo, $packageThree, $packageFour));
+
+//Função principal para gerar as combinações
+generateCombinations($packageOne, $packageTwo, $packageThree, $packageFour, $packageOne);
--- index.php
+++ index.php
@@ -1,42 +1,116 @@
 <?php
 
 function dd(...$args) {
+    echo '<pre>';
     foreach ($args as $arg) {
-        var_dump($arg);
+        var_dump(print_r($arg));
     }
-
+    echo '</pre>';
     die();
 }
 
+$packageOne = [
+    "package" => "internet",
+    "ids" => [1, 2, 3, 4, 5]
+];
 
+$packageTwo = [
+    "package" => "tv",
+    "ids" => [21, 22, 23, 24, 25]
+];
 
+$packageThree = [
+    "package" => "cell",
+    "ids" => [31, 32, 33, 34, 35]
+];
 
+$packageFour = [
+    "package" => "cell",
+    "ids" => [41, 42, 43, 44, 45]
+];
 
+//Variavel será utilizada para definir o tamanho da primeira coluna
+$base_array_length = 0;
 
-function show($i, $array) {
-    if ($i != $array[0]) {
-        echo $i.$array[0].' ';
+function combinateBaseColumn($result_combinations_packages, $rest_packages, $external_loop, $internal_loop)
+{
+//    Variável para não permitir que exceda o índice da coluna base
+    global $base_array_length;
+
+//    Índice da coluna base para atribuir as combinações
+    $index = 0;
+
+//    Loop exponencial externo
+    for ($i = 0; $i < $external_loop; $i++) {
+        foreach ($rest_packages[0]['ids'] as $rest) {
+//            Loop exponencial interno
+            for ($j = 0; $j < $internal_loop; $j++) {
+                if ($index < $base_array_length) {
+                    $result_combinations_packages[$index] .= ' | ' . $rest_packages[0]['package'] . ', ' . $rest;
+                }
+
+                $index++;
+            }
+        }
     }
 
-    array_shift($array);
+//    Recalcula os loops exponenciais para ser utilizado caso esta função seja chamada recursivamente
+    $external_loop *= 5;
+    $internal_loop /= 5;
 
-    if ($array) {
-        show($i, $array);
+//    Retira o item corrente do resto dos pacotes
+    array_shift($rest_packages);
+
+//    Se tiver pacotes restantes chama esta função recursivamente
+    if ($rest_packages) {
+        combinateBaseColumn($result_combinations_packages, $rest_packages, $external_loop, $internal_loop);
+    } else {
+//        Término do processo, imprime na tela o resultado das combinações
+        dd($result_combinations_packages);
     }
 }
 
-function combinations(...$combinations) {
-    echo 'Qtd: '.count($combinations[0]).'<br/><br/><br/>';
 
-    foreach ($combinations[0] as $combination) {
-        show($combination, $combinations[0]);
+
+function generateCombinations(...$packages)
+{
+//    Tamanho base da primeira coluna
+    global $base_array_length;
+
+//    Array da primeira coluna
+    $first_package = [];
+
+//    Pega a quantidade de pacotes tirando o da primeira coluna
+    $count_packages = count($packages) -1;
+
+//    Pega a quantidade de itens de cada pacote no caso 5
+    $count_package_id = count($packages[0]['ids']);
+
+//    Faz o calculo da exponenciação para gerar o loop conforme a quantidade de pacotes tirando o da primeira coluna
+    $exp = pow($count_package_id, $count_packages);
+
+//    Realiza o loop nos itens do primeiro pacote que é o base
+    foreach ($packages[0]['ids'] as $id) {
+        for ($i = 0; $i < $exp; $i++) {
+            $first_package[] = $packages[0]['package'].' - '.  $id;
+        }
     }
+
+//    Exclui a primeira coluna dos packages
+    array_shift($packages);
+
+//    Calculo exponencial dos loops para atribuir a coluna base
+//    Loop externo do array
+    $external_loop = count($packages) > 1 ? pow(5, count($packages) -1) : 5;
+//    Loop interno do array
+    $internal_loop = pow(5, count($packages) -1);
+//    Seta o tamanho da coluna base
+    $base_array_length = count($first_package);
+
+//    Função para atribuir a coluna base as combinações
+    combinateBaseColumn($first_package, $packages, $external_loop, $internal_loop);
 }
 
 
-$combination = [1, 2, 3, 5, 6, 7];
-combinations($combination);
-
-
-
-
+//Função principal para gerar as combinações
+generateCombinations($packageOne, $packageTwo, $packageThree, $packageFour, $packageOne);
